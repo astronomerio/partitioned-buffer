5aa74ff304d146a6efa385532b8aa961f72436df
Alois Barreras <alois@vicino.io>
2016-09-21 19:08:02 -0400

    moving functionality from sub buffer to main buffer, adding tests for everything

--- test/test.js
+++ test/test.js
@@ -1,11 +1,2 @@
-import sinon from 'sinon';
-import test from 'blue-tape';
-import SubBuffer from '../lib/sub-buffer';
-
-test('buffer pushes data', (t) => {
-    const subBuffer = new SubBuffer();
-    const data = ['foo', 'bar'];
-    subBuffer.push(data);
-    t.equal(data, subBuffer.records);
-});
-
+import './partitioned-buffer';
+// import './sub-buffer';
